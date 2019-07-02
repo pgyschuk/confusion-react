77cf9043c8846dafccf696abdd9f0289f73dca2b
Peter Gyschuk <peter.gyschuk@jivesoftware.com>
2019-07-02 16:52:52 +0300

    Single Page Application part 1

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,95 +1,43 @@
-export const DISHES = [
-  {
-    id: 0,
-    name: 'Uthappizza',
-    image: 'assets/images/uthappizza.png',
-    category: 'mains',
-    label: 'Hot',
-    price: '4.99',
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-    comments:[
+export const DISHES =
+    [
+      {
+        id: 0,
+        name:'Uthappizza',
+        image: '/assets/images/uthappizza.png',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+      },
       {
         id: 1,
-        datetime: '05/29/2015 05:50 AM',
-        author:'John Doe',
-        text:"This Uthappizza is Very Good!"
+        name:'Zucchipakoda',
+        image: '/assets/images/zucchipakoda.png',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
       },
       {
         id: 2,
-        datetime: '05/29/2016 08:40 AM',
-        author:'John Gold',
-        text:"I like Uthappizza in this restourant!"
-      }
-    ]
-  },
-  {
-    id: 1,
-    name: 'Zucchipakoda',
-    image: 'assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-    comments:[
+        name:'Vadonut',
+        image: '/assets/images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+      },
       {
         id: 3,
-        datetime: '05/29/2017 07:50 PM',
-        author:'John Doe',
-        text:"This Zucchipakoda is Very Good!"
-      },
-      {
-        id: 4,
-        datetime: '05/29/2019 01:24 PM',
-        author:'John Gold',
-        text:"I like Zucchipakoda in this restourant!"
+        name:'ElaiCheese Cake',
+        image: '/assets/images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
       }
-    ]
-  },
-  {
-    id: 2,
-    name: 'Vadonut',
-    image: 'assets/images/vadonut.png',
-    category: 'appetizer',
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
-    comments:[
-      {
-        id: 5,
-        datetime: '06/22/2016 07:34 AM',
-        author:'John Doe',
-        text:"This Vadonut is Very Good!"
-      },
-      {
-        id: 6,
-        datetime: '05/29/2017 05:59 AM',
-        author:'John Gold',
-        text:"I like Vadonut in this restourant!"
-      }
-    ]
-  },
-  {
-    id: 3,
-    name: 'ElaiCheese Cake',
-    image: 'assets/images/elaicheesecake.png',
-    category: 'dessert',
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-    comments:[
-      {
-        id: 7,
-        datetime: '05/12/2017 08:45 AM',
-        author:'John Doe',
-        text:"This ElaiCheese Cake is Very Good!"
-      },
-      {
-        id: 8,
-        datetime: '05/29/2019 05:50 AM',
-        author:'John Gold',
-        text:"I like ElaiCheese Cake in this restourant!"
-      }
-    ]
-  }
-
-]
+    ];
